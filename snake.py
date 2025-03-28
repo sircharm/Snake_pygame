2f2699be7011404acd286707e9394ee44164dabe
Marcelo Bernardes de Oliveira <marcelo.web13@gmail.com>
2025-03-28 20:14:48 -0300

    Merge branch 'main' of https://github.com/sircharm/Snake_pygame

--- snake.py
+++ snake.py
@@ -9,6 +9,9 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
 
@@ -17,12 +20,23 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-  screen.fill("black")
+  screen.fill("white")
   
   #game goes here
+  pygame.draw.circle(screen, "black", player_pos, 40)
+
+  keys = pygame.key.get_pressed()
+  if keys[pygame.K_w]:
+      player_pos.y -= 300 * dt
+  if keys[pygame.K_s]:
+      player_pos.y += 300 * dt
+  if keys[pygame.K_a]:
+      player_pos.x -= 300 * dt
+  if keys[pygame.K_d]:
+      player_pos.x += 300 * dt
   
   pygame.display.flip()
   
-  clock.tick(60) #60 FPS
+  dt = clock.tick(60) / 1000 #60 FPS
 
 pygame.quit()
