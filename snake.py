62cfbfb15d39d07a455041d9241df2ccd2373936
Marcelo Bernardes de Oliveira <marcelo.web13@gmail.com>
2025-03-28 19:36:57 -0300

    Update snake.py
    Very basic code just to test if the dependencies were installed

--- snake.py
+++ snake.py
@@ -1,5 +1,28 @@
-
-
+import pygame
 
 
 #Simple code for creating a pygame Snake
+# I'm taking example code from the pygame documentation to start
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+
+  #checks if the X button was pressed and quits the main loop if so
+  for event in pygame.event.get():
+    if event.type == pygame.quit():
+      running = False
+
+  screen.fill("white")
+  
+  #game goes here
+  
+  pygame.display.flip()
+  
+  clock.tick(60) #60 FPS
+
+pygame.quit()
