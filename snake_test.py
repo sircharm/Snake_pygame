2da67e034540c40872cd963ed166fd3a705677d3
Marcelo Bernardes de Oliveira <marcelo.web13@gmail.com>
2025-03-29 09:19:13 -0300

    Create snake_test.py
    File to make tests in the future. I'm creating it now just so pytest won't raise an error

---
+++ snake_test.py
@@ -0,0 +1,6 @@
+#File I'll use to run tests on the code with pytest
+
+#I'm going to put a test that's always true here just so pytest won't raise an error
+#It raises an error when there are no tests
+
+assert 2 + 2 == 4
